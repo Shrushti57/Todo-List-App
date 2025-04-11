961c1bbb41f732598310b0dd67870777ed3f055e
Shrushti57 <shrushtikunjir5757@gmail.com>
2025-04-11 09:59:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputbox = document.getElementById("inputbox");
+const listcontainer = document.getElementById("listcontainer");
+
+function addTask(){
+    if(inputbox.value === ''){
+        alert("You Must Write Something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = " ";
+    savedata()
+}
+
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
+
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showtask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
